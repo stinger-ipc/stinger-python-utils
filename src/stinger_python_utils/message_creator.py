5ba5a4c3f60987f4168299839ec30f146ad8a836
Jacob Brunson <github@jacobbrunson.com>
2026-06-30 08:22:40 -0600

    chore: bump version to 0.1.10

--- src/stinger_python_utils/message_creator.py
+++ src/stinger_python_utils/message_creator.py
@@ -16,27 +16,46 @@ class MessageCreator:
 
     @classmethod
     def signal_message(cls, topic: str, payload: BaseModel) -> Message:
+        return cls.binary_signal_message(
+            topic,
+            payload.model_dump_json(by_alias=True).encode("utf-8"),
+            "application/json",
+        )
+
+    @classmethod
+    def binary_signal_message(cls, topic: str, payload: bytes, content_type: str) -> Message:
         cls._validate_topic(topic)
         return Message(
             topic=topic,
-            payload=payload.model_dump_json(by_alias=True).encode("utf-8"),
+            payload=payload,
             qos=1,
             retain=False,
-            content_type="application/json",
+            content_type=content_type,
         )
 
     @classmethod
     def status_message(
-        cls, topic, status_message: BaseModel, expiry_seconds: int
+        cls, topic: str, status_message: BaseModel, expiry_seconds: int
+    ) -> Message:
+        return cls.binary_status_message(
+            topic,
+            status_message.model_dump_json(by_alias=True).encode("utf-8"),
+            "application/json",
+            expiry_seconds=expiry_seconds,
+        )
+
+    @classmethod
+    def binary_status_message(
+        cls, topic: str, payload: bytes, content_type: str, expiry_seconds: int
     ) -> Message:
         cls._validate_topic(topic)
         return Message(
             topic=topic,
-            payload=status_message.model_dump_json(by_alias=True).encode("utf-8"),
+            payload=payload,
             qos=1,
             retain=True,
             message_expiry_interval=expiry_seconds,
-            content_type="application/json",
+            content_type=content_type,
         )
 
     @classmethod
@@ -86,19 +105,32 @@ class MessageCreator:
         """
         This could be used for a successful response to a request.
         """
-        cls._validate_topic(response_topic, "response_topic")
         if isinstance(response_obj, BaseModel):
             payload = response_obj.model_dump_json(by_alias=True).encode("utf-8")
         elif isinstance(response_obj, str):
             payload = response_obj.encode("utf-8")
         else:
             payload = response_obj
+        return cls.binary_response_message(
+            response_topic, payload, "application/json", return_code, correlation_id
+        )
+
+    @classmethod
+    def binary_response_message(
+        cls,
+        response_topic: str,
+        payload: bytes,
+        content_type: str,
+        return_code: Union[int, MethodReturnCode],
+        correlation_id: Union[str, bytes, None] = None,
+    ) -> Message:
+        cls._validate_topic(response_topic, "response_topic")
         rc = (
             return_code.value
             if isinstance(return_code, MethodReturnCode)
             else return_code
         )
-        msg_obj = Message(
+        return Message(
             topic=response_topic,
             payload=payload,
             qos=1,
@@ -109,9 +141,8 @@ class MessageCreator:
                 else correlation_id
             ),
             user_properties={"ReturnCode": str(rc)},
-            content_type="application/json",
+            content_type=content_type,
         )
-        return msg_obj
 
     @classmethod
     def property_state_message(
@@ -120,13 +151,24 @@ class MessageCreator:
         """
         Creates a retained message representing the state/value of a property.
         """
+        return cls.binary_property_state_message(
+            topic,
+            state_obj.model_dump_json(by_alias=True).encode("utf-8"),
+            "application/json",
+            state_version,
+        )
+
+    @classmethod
+    def binary_property_state_message(
+        cls, topic: str, payload: bytes, content_type: str, state_version: Optional[int] = None
+    ) -> Message:
         cls._validate_topic(topic)
         msg_obj = Message(
             topic=topic,
-            payload=state_obj.model_dump_json(by_alias=True).encode("utf-8"),
+            payload=payload,
             qos=1,
             retain=True,
-            content_type="application/json",
+            content_type=content_type,
         )
         if state_version is not None:
             msg_obj.user_properties = {"PropertyVersion": str(state_version)}
@@ -144,14 +186,33 @@ class MessageCreator:
         """
         Creates a message representing a request to update a property.
         """
+        return cls.binary_property_update_request_message(
+            topic,
+            property_obj.model_dump_json(by_alias=True).encode("utf-8"),
+            "application/json",
+            version,
+            response_topic,
+            correlation_id,
+        )
+
+    @classmethod
+    def binary_property_update_request_message(
+        cls,
+        topic: str,
+        payload: bytes,
+        content_type: str,
+        version: str,
+        response_topic: str,
+        correlation_id: Union[str, bytes, None] = None,
+    ) -> Message:
         cls._validate_topic(topic)
         cls._validate_topic(response_topic, "response_topic")
-        msg_obj = Message(
+        return Message(
             topic=topic,
-            payload=property_obj.model_dump_json(by_alias=True).encode("utf-8"),
+            payload=payload,
             qos=1,
             retain=False,
-            content_type="application/json",
+            content_type=content_type,
             response_topic=response_topic,
             correlation_data=(
                 correlation_id.encode("utf-8")
@@ -160,7 +221,6 @@ class MessageCreator:
             ),
             user_properties={"PropertyVersion": str(version)},
         )
-        return msg_obj
 
     @classmethod
     def property_response_message(
@@ -175,6 +235,27 @@ class MessageCreator:
         """
         Creates a message representing a response to a property update request.
         """
+        return cls.binary_property_response_message(
+            response_topic,
+            property_obj.model_dump_json(by_alias=True).encode("utf-8"),
+            "application/json",
+            version,
+            return_code,
+            correlation_id,
+            debug_info,
+        )
+
+    @classmethod
+    def binary_property_response_message(
+        cls,
+        response_topic: str,
+        payload: bytes,
+        content_type: str,
+        version: str,
+        return_code: Union[int, MethodReturnCode],
+        correlation_id: Union[str, bytes, None] = None,
+        debug_info: Optional[str] = None,
+    ) -> Message:
         cls._validate_topic(response_topic, "response_topic")
         rc = (
             return_code.value
@@ -183,10 +264,10 @@ class MessageCreator:
         )
         msg_obj = Message(
             topic=response_topic,
-            payload=property_obj.model_dump_json(by_alias=True).encode("utf-8"),
+            payload=payload,
             qos=1,
             retain=False,
-            content_type="application/json",
+            content_type=content_type,
             correlation_data=(
                 correlation_id.encode("utf-8")
                 if isinstance(correlation_id, str)
@@ -211,21 +292,37 @@ class MessageCreator:
         response_topic: str,
         correlation_id: Union[str, bytes, None] = None,
     ) -> Message:
+        return cls.binary_request_message(
+            topic,
+            request_obj.model_dump_json(by_alias=True).encode("utf-8"),
+            "application/json",
+            response_topic,
+            correlation_id,
+        )
+
+    @classmethod
+    def binary_request_message(
+        cls,
+        topic: str,
+        payload: bytes,
+        content_type: str,
+        response_topic: str,
+        correlation_id: Union[str, bytes, None] = None,
+    ) -> Message:
         cls._validate_topic(topic)
         cls._validate_topic(response_topic, "response_topic")
         if correlation_id is None:
             correlation_id = str(uuid.uuid4())
-        msg_obj = Message(
+        return Message(
             topic=topic,
-            payload=request_obj.model_dump_json(by_alias=True).encode("utf-8"),
+            payload=payload,
             qos=1,
             retain=False,
             response_topic=response_topic,
-            content_type="application/json",
+            content_type=content_type,
             correlation_data=(
                 correlation_id.encode("utf-8")
                 if isinstance(correlation_id, str)
                 else correlation_id
             ),
         )
-        return msg_obj
